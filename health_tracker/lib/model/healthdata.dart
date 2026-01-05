0b502c2c070e85f4d88dffd7c9dcdf7848a1e9a0
webdeconline <webdeconline@gmail.com>
2026-01-05 23:17:24 +0100

    update

--- health_tracker/lib/model/healthdata.dart
+++ health_tracker/lib/model/healthdata.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class HealthData extends ChangeNotifier {
-  String _weight = '72';
-  String _heartRate = '68';
+  String _weight = '72'; //stanbaard gewicht in kg
+  String _heartRate = '68'; //standaard hartslag in bpm
 
-  String get weight => _weight;
-  String get heartRate => _heartRate;
+  String get weight => _weight; //gewicht ophalen
+  String get heartRate => _heartRate; //hartslag ophalen
 
   void updateWeight(String newWeight) {
     _weight = newWeight;
     notifyListeners();
-  }
+  } //gewicht bijwerken en luisteraars op de hoogte stellen
 
   void updateHeartRate(String newHeartRate) {
     _heartRate = newHeartRate;
     notifyListeners();
-  }
+  } //hartslag bijwerken en luisteraars op de hoogte stellen
 }
